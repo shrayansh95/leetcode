338fd852a8f33d4065ef6da67c0ddf1b7f41d5f4
Shrayansh Baunthiyal <72238316+shrayansh95@users.noreply.github.com>
2022-05-05 07:08:45 +0530

    Time: 0 ms (100.00%), Space: 41.6 MB (63.48%) - LeetHub

--- 225-implement-stack-using-queues/225-implement-stack-using-queues.java
+++ 225-implement-stack-using-queues/225-implement-stack-using-queues.java
@@ -1,30 +1,37 @@
 class MyStack {
-    Queue<Integer> q1, q2;
-
+    // Queue<Integer> q1, q2;
+    Queue<Integer> queue;
+    
+    
     public MyStack() {
-        q1 = new LinkedList<>();
-        q2 = new LinkedList<>();
+        // q1 = new LinkedList<>();
+        // q2 = new LinkedList<>();
+        queue = new LinkedList<>();
     }
     
     public void push(int x) {
-        q2.add(x);
-        while (!q1.isEmpty()) 
-            q2.add(q1.poll());
-        Queue<Integer> q = q1;
-        q1 = q2;
-        q2 = q;
+        // q2.add(x);
+        // while (!q1.isEmpty()) 
+        //     q2.add(q1.poll());
+        // Queue<Integer> q = q1;
+        // q1 = q2;
+        // q2 = q;
+        queue.add(x);
+        int n = queue.size();
+        for (int i = 1; i < n; ++i) 
+            queue.add(queue.poll());
     }
     
     public int pop() {
-        return q1.poll();
+        return queue.poll();
     }
     
     public int top() {
-        return q1.peek();
+        return queue.peek();
     }
     
     public boolean empty() {
-        return q1.isEmpty();
+        return queue.isEmpty();
     }
 }
 

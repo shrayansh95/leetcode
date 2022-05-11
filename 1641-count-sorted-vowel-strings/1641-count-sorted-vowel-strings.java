8eb0eebd237eebff4d5ff38668fb4610c0c6441f
Shrayansh Baunthiyal <72238316+shrayansh95@users.noreply.github.com>
2022-05-11 07:47:23 +0530

    Time: 0 ms (100.00%), Space: 40.6 MB (61.67%) - LeetHub

---
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int countVowelStrings(int n) {
+        int a = 1, e = 1, i = 1, o = 1, u = 1;
+        for (int j = 2; j <= n; ++j) {
+            a += (e + i + o + u);
+            e += (i + o + u);
+            i += (o + u);
+            o += u;
+        }
+        return (a + e + i + o + u);
+    }
+}

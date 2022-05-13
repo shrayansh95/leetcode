437129aa3ad1a9597bf7d7dd848028bfacf18d4f
Shrayansh Baunthiyal <72238316+shrayansh95@users.noreply.github.com>
2022-05-13 07:38:14 +0530

    Time: 0 ms (100.00%), Space: 45.2 MB (34.72%) - LeetHub

--- 117-populating-next-right-pointers-in-each-node-ii/117-populating-next-right-pointers-in-each-node-ii.java
+++ 117-populating-next-right-pointers-in-each-node-ii/117-populating-next-right-pointers-in-each-node-ii.java
@@ -23,25 +23,22 @@ class Node {
 
 class Solution {
     public Node connect(Node root) {
-        if (root != null) {
-            Queue<Node> q = new LinkedList<>();
-            q.add(root);
-            Node temp;
-            int size;
-            while (!q.isEmpty()) {
-                size = q.size();
-                for (int i = 0; i < size; ++i) {
-                    temp = q.poll();
-                    if (temp.left != null) 
-                        q.add(temp.left);
-                    if (temp.right != null)
-                        q.add(temp.right);
-                    if (i == size - 1)
-                        temp.next = null;
-                    else
-                        temp.next = q.peek();
+        Node node = root, nextLevelHead = new Node(0), needle;
+        while (node != null) { //loop for all the levels
+            needle = nextLevelHead;
+            while (node != null) { // traveling levelwise through all the nodes
+                if (node.left != null) {
+                    needle.next = node.left;
+                    needle = needle.next;
                 }
+                if (node.right != null) {
+                    needle.next = node.right;
+                    needle = needle.next;
+                }
+                node = node.next;
             }
+            node = nextLevelHead.next; // go to the next level
+            nextLevelHead.next = null;
         }
         return root;
     }

35b480185ea06c7f44bf5120f8e54673112b8a9c
Shrayansh Baunthiyal <72238316+shrayansh95@users.noreply.github.com>
2022-05-14 11:04:38 +0530

    Time: 0 ms (100.00%), Space: 45.8 MB (33.64%) - LeetHub

--- 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.java
+++ 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.java
@@ -15,20 +15,28 @@ class Node {
 
 class Solution {
     public Node copyRandomList(Node head) {
-        Node temp = head;
-        Map<Node, Node> map = new HashMap<>();
-        while (temp != null) {
-            map.put(temp, new Node(temp.val));
-            temp = temp.next;
+        Node iter = head, copy, dummy = new Node(0);
+        // Creating deep copies besides the original nodes 
+        while (iter != null) {
+            copy = new Node(iter.val);
+            copy.next = iter.next;
+            iter.next = copy;
+            iter = iter.next.next;
         }
-        temp = head;
-        while (temp != null) {
-            if (temp.next != null) 
-                map.get(temp).next = map.get(temp.next);
-            if (temp.random != null)
-                map.get(temp).random = map.get(temp.random);
-            temp = temp.next;
+        iter = head;
+        while (iter != null) {
+            if (iter.random != null)
+                iter.next.random = iter.random.next;
+            iter = iter.next.next;
         }
-        return map.get(head);
+        iter = head;
+        Node temp = dummy;
+        while (iter != null) {
+            temp.next = iter.next;
+            iter.next = iter.next.next;
+            temp = temp.next;
+            iter = iter.next;
+        }
+        return dummy.next;
     }
 }

500011db58213cd080f6844e659e760395c1aa75
Shrayansh Baunthiyal <72238316+shrayansh95@users.noreply.github.com>
2022-05-16 06:39:40 +0530

    Time: 56 ms (16.87%), Space: 73.1 MB (10.33%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
@@ -6,31 +6,27 @@ class Solution {
         boolean visited[][] = new boolean[n][n];
         Queue<int[]> q = new LinkedList<>();
         q.add(new int[]{0, 0, 1});
-        int size;
         int temp[];
         int row, col, nrow, ncol, cost;
         int dir[][] = new int[][]{{-1, 0}, {-1, 1}, {0, 1}, {1, 1}, 
                                 {1, 0}, {1, -1}, {0, -1}, {-1, -1}};
         while (!q.isEmpty()) {
-            size = q.size();
-            for (int i = 0; i < size; ++i) {
-                temp = q.remove();
-                row = temp[0];
-                col = temp[1];
-                cost = temp[2];
-                if (visited[row][col])
+            temp = q.remove();
+            row = temp[0];
+            col = temp[1];
+            cost = temp[2];
+            if (visited[row][col])
+                continue;
+            visited[row][col] = true;
+            if (row == n - 1 && col == n - 1)
+                return cost;
+            for (int k = 0; k < 8; ++k) {
+                nrow = row + dir[k][0];
+                ncol = col + dir[k][1];
+                if (nrow < 0 || ncol < 0 || nrow >= n || ncol >= n || grid[nrow][ncol] == 1)
                     continue;
-                visited[row][col] = true;
-                if (row == n - 1 && col == n - 1)
-                    return cost;
-                for (int k = 0; k < 8; ++k) {
-                    nrow = row + dir[k][0];
-                    ncol = col + dir[k][1];
-                    if (nrow < 0 || ncol < 0 || nrow >= n || ncol >= n || grid[nrow][ncol] == 1)
-                        continue;
-                    q.add(new int[]{nrow, ncol, cost + 1});
-                }
-            }
+                q.add(new int[]{nrow, ncol, cost + 1});
+            }  
         }
         return -1;
     }

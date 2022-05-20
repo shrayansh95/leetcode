ef416298265ebf3cbf2e4228b15320de7cd101e3
Shrayansh Baunthiyal <72238316+shrayansh95@users.noreply.github.com>
2022-05-20 06:57:53 +0530

    Time: 1 ms (52.25%), Space: 42.5 MB (29.46%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.java
+++ 63-unique-paths-ii/63-unique-paths-ii.java
@@ -2,13 +2,12 @@ class Solution {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
         int m = obstacleGrid.length, n = obstacleGrid[0].length;
         int dp[][] = new int[m][n];
+        int prevT = 0, prevL = 0, cur;
         for (int i = 0; i < m; ++i) {
             for (int j = 0; j < n; ++j) {
                 if (obstacleGrid[i][j] == 1) {
                     dp[i][j] = 0;
-                    continue;
-                }
-                if (i == 0 && j == 0)
+                } else if (i == 0 && j == 0)
                     dp[i][j] = 1;
                 else {
                     int top = 0, left = 0;

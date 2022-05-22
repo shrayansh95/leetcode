4235165ec71202592cbaff0af04e20b065d1895c
Shrayansh Baunthiyal <72238316+shrayansh95@users.noreply.github.com>
2022-05-22 09:38:54 +0530

    Time: 527 ms (66.67%), Space: 100.8 MB (33.33%) - LeetHub

--- 2280-minimum-lines-to-represent-a-line-chart/2280-minimum-lines-to-represent-a-line-chart.cpp
+++ 2280-minimum-lines-to-represent-a-line-chart/2280-minimum-lines-to-represent-a-line-chart.cpp
@@ -2,8 +2,6 @@ class Solution {
 public:
     int minimumLines(vector<vector<int>>& stockPrices) {
         int n = stockPrices.size();
-        if (n == 1)
-            return 0;
         int count = 0;
         sort(stockPrices.begin(), stockPrices.end());
         long double prev = -1e9, cur = 0, dx = 0.0, dy = 0.0;

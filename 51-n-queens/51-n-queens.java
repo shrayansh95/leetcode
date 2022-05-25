5fdda28616608abe6205a4ff8ad0252fe293a129
Shrayansh Baunthiyal <72238316+shrayansh95@users.noreply.github.com>
2022-05-25 08:38:17 +0530

    Time: 2 ms (97.21%), Space: 42.6 MB (86.39%) - LeetHub

--- 51-n-queens/51-n-queens.java
+++ 51-n-queens/51-n-queens.java
@@ -1,42 +1,31 @@
 class Solution {
     public List<List<String>> solveNQueens(int n) {
+        boolean cols[] = new boolean[n], left[] = new boolean[2 * n - 1], right[] = new boolean[2 * n - 1];
         List<List<String>> ans = new ArrayList<>();
-        char board[][] = new char[n][n];
-        for (char it[] : board) 
-            Arrays.fill(it, '.');
-        solve(board, 0, n, ans);
+        solve(0, n, cols, left, right, new ArrayList<>(), ans);
         return ans;
     }
     
-    private void solve(char board[][], int row, int n, List<List<String>> ans) {
+    private void solve(int row, int n, boolean cols[], boolean left[], boolean right[], List<String> temp, List<List<String>> ans) {
         if (row == n) {
-            ans.add(construct(board));
+            ans.add(new ArrayList<>(temp));
             return;
         }
         for (int col = 0; col < n; ++col) {
-            if (valid(board, row, col, n)) {
-                board[row][col] = 'Q';
-                solve(board, row + 1, n, ans);
-                board[row][col] = '.';
+            if (!cols[col] && !right[col + row] && !left[n - 1 + col - row]) {
+                char[] charArray = new char[n];
+                Arrays.fill(charArray, '.');
+                charArray[col] = 'Q';
+                temp.add(new String(charArray));
+                cols[col] = true;
+                right[col + row] = true;
+                left[n - 1 + col - row] = true;
+                solve(row + 1, n, cols, left, right, temp, ans);
+                temp.remove(temp.size() -1);
+                cols[col] = false;
+                right[col + row] = false;
+                left[n - 1 + col - row] = false;
             }
         }
-    }
-    
-    private boolean valid(char board[][], int row, int col, int n) {
-        for (int i = 0; i < row; ++i) {
-            for (int j = 0; j < n; ++j) {
-                if (board[i][j] == 'Q' && ((j == col) || (row + j == col + i) || (row + col == i + j)))
-                    return false;
-            }
-        }
-        return true;
-    } 
-    
-    private List<String> construct(char board[][]) {
-        List<String> temp = new ArrayList<>();
-        for (char it[] : board) {
-            temp.add(new String(it));
-        }
-        return temp;
     }
 }

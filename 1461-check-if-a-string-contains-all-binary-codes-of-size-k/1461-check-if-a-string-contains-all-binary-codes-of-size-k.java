905e1240f5d9d2ed785ba6231570aa259368dbfa
Shrayansh Baunthiyal <72238316+shrayansh95@users.noreply.github.com>
2022-05-31 07:24:56 +0530

    Time: 106 ms (73.44%), Space: 75.2 MB (52.08%) - LeetHub

--- 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.java
+++ 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.java
@@ -8,11 +8,11 @@ class Solution {
         for(int i = 0; i < k; i++)
             val = (val << 1) | (s.charAt(i) - '0');
         set.add(val);
+        int mask;
          for(int i = k; i < n && set.size() < totalSize; i++) {
              val = (val << 1) | (s.charAt(i) - '0');
-             int mask = (1 << k);
+             mask = (1 << k);
              val &= (~mask);
-             // val -= ((s.charAt(i - k) - '0') << k);
              set.add(val);
         }
         return set.size() == totalSize;

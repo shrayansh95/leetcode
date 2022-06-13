b3d4f1893e767e6070f1eda4f3a2402fc65ce896
Shrayansh Baunthiyal <72238316+shrayansh95@users.noreply.github.com>
2022-06-13 05:55:00 +0530

    Time: 1 ms (99.88%), Space: 42.3 MB (77.71%) - LeetHub

--- 120-triangle/120-triangle.java
+++ 120-triangle/120-triangle.java
@@ -4,11 +4,12 @@ class Solution {
         int dp[][] = new int[n][n];
         for (int it[] : dp)
             Arrays.fill(it, -1);
+        
         return solve(0, 0, triangle, n, dp);
     }
     private int solve(int r, int c, List<List<Integer>> triangle, int n, int dp[][]) {
-        if (r == n)
-            return 0;
+        if (r == n - 1)
+            return triangle.get(r).get(c);
         if (dp[r][c] != -1)
             return dp[r][c];
         return dp[r][c] = triangle.get(r).get(c) + Math.min(solve(r + 1, c, triangle, n, dp), solve(r + 1, c + 1, triangle, n, dp));

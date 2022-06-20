590ad0b63bc2fbd93c91a61b8ddf4cf460a50149
Shrayansh Baunthiyal <72238316+shrayansh95@users.noreply.github.com>
2022-06-20 08:27:12 +0530

    Time: 30 ms (69.94%), Space: 53.8 MB (71.17%) - LeetHub

---
+++ 820-short-encoding-of-words/820-short-encoding-of-words.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int minimumLengthEncoding(String[] words) {
+        Set<String> set = new HashSet<>(Arrays.asList(words));
+        for (String s : words) {
+            for (int i = 1; i < s.length(); ++i)
+                set.remove(s.substring(i));
+        }
+        int ans = 0;
+        for (String s : set)
+            ans += (s.length() + 1);
+        return ans;
+    }
+}

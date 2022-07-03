d2fcdce88db856deaa1c1a0a0c04e1ef4b7dcae1
Shrayansh Baunthiyal <72238316+shrayansh95@users.noreply.github.com>
2022-07-03 10:33:38 +0530

    Time: 4 ms (100.00%), Space: 40.4 MB (77.78%) - LeetHub

--- 2327-number-of-people-aware-of-a-secret/2327-number-of-people-aware-of-a-secret.java
+++ 2327-number-of-people-aware-of-a-secret/2327-number-of-people-aware-of-a-secret.java
@@ -1,7 +1,13 @@
 class Solution {
     public int peopleAwareOfSecret(int n, int delay, int forget) {
+        // Represents the number of people who got to know the secret
+        // on the ith day
         int dp[] = new int[n + 1];
+        // The first day only one person knows the secret 
         dp[1] = 1;
+        // Share stores the number of people who are going to share 
+        // the secret on the ith day which is also equal
+        // to the number of people who get to know the secret today
         int share = 0;
         int mod = (int)(1e9 + 7);
         for (int i = 2; i <= n; ++i) {
@@ -10,8 +16,11 @@ class Solution {
             dp[i] = share;
         }
         int ans = 0;
+        // We take the last forget days in consideration to get the ans
+        // because it has only those number of people who remembers the
+        // secret right now 
         for (int i = n - forget + 1; i <= n; ++i)
-            ans = (int)((ans * 1L + dp[i]) % mod);
+            ans = (int)((ans + dp[i]) % mod);
         return ans;
     }
 }

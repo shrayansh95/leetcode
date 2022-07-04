2a9b936be50a21c05e5ee55d0a33e3c8a0cc14e9
Shrayansh Baunthiyal <72238316+shrayansh95@users.noreply.github.com>
2022-07-04 09:50:34 +0530

    Time: 3 ms (86.84%), Space: 51.5 MB (66.42%) - LeetHub

--- 135-candy/135-candy.java
+++ 135-candy/135-candy.java
@@ -1,16 +1,37 @@
 class Solution {
     public int candy(int[] ratings) {
         int n = ratings.length;
-        int ans = 0, candies[] = new int[n];
-        Arrays.fill(candies, 1);
-        for (int i = 1; i < n; ++i) 
-            if (ratings[i] > ratings[i - 1])
-                candies[i] = candies[i - 1] + 1;
-        for (int i = n - 1; i > 0; --i) 
-            if (ratings[i - 1] > ratings[i])
-                candies[i - 1] = Math.max(candies[i - 1], candies[i] + 1);
-        for (int i = 0; i < n; ++i)
-            ans += candies[i];
+        int i = 0,
+        s = 0, // stores the start index of the left or right sides of the peak
+        ans = 1; //initialize as 1 because in each round we minus 1 for the left 
+                 // bottom since it was calculated once in the previous round
+        int left, right; // to store the length of the respective halves
+        int peak; // stores the candies in peak element
+        while (i < n - 1) {
+            // min to peak element 
+            while (i < n - 1 && ratings[i] < ratings[i + 1])
+                ++i;
+            // size of left half 
+            left = i - s;
+            s = i;
+            // peak to min element 
+            while (i < n - 1 && ratings[i] > ratings[i + 1])
+                ++i;
+            // size of right half 
+            right = i - s;
+            s = i;
+            peak = Math.max(left, right) + 1;
+            // the minus 1 is because we are taking the previous min from 
+            // prev right half 
+            ans += (left * (left + 1)) / 2 + (right * (right + 1)) / 2 
+                    + peak - 1;
+            // Add 1 for each duplicate element 
+            while (i < n - 1 && ratings[i] == ratings[i + 1]) {
+                ++i;
+                ++ans;
+            }
+            s = i;
+        }
         return ans;
     }
 }

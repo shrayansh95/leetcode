36eb17cc580bbd35458a4e03ae24832cecf5dd90
Shrayansh Baunthiyal <72238316+shrayansh95@users.noreply.github.com>
2022-07-05 13:44:39 +0530

    Time: 0 ms (100.00%), Space: 42.5 MB (18.02%) - LeetHub

--- 165-compare-version-numbers/165-compare-version-numbers.java
+++ 165-compare-version-numbers/165-compare-version-numbers.java
@@ -1,17 +1,17 @@
 class Solution {
     public int compareVersion(String version1, String version2) {
         int v1 = 0, v2 = 0, n = version1.length(), m = version2.length();
-        String a = "", b = "";
+        int a = 0, b = 0;
         while (v1 < n || v2 < m) {
-            a = "0";
-            b = "0";
+            a = 0;
+            b = 0;
             while (v1 < n && version1.charAt(v1) != '.') 
-                a += version1.charAt(v1++);
+                a = (a * 10) + (version1.charAt(v1++) - '0');
             while (v2 < m && version2.charAt(v2) != '.')
-                b += version2.charAt(v2++);
-            if (Integer.valueOf(a) < Integer.valueOf(b))
+                b = (b * 10) + (version2.charAt(v2++) - '0');
+            if (a < b)
                 return -1;
-            else if (Integer.valueOf(a) > Integer.valueOf(b))
+            else if (a > b)
                 return 1;
             ++v1;
             ++v2;

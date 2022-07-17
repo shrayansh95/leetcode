7e2a0104eeb0ca42eaa23990a35036e02eda0e10
Shrayansh Baunthiyal <72238316+shrayansh95@users.noreply.github.com>
2022-07-17 10:36:54 +0530

    Time: 35 ms (57.83%), Space: 47.2 MB (85.54%) - LeetHub

--- 629-k-inverse-pairs-array/629-k-inverse-pairs-array.java
+++ 629-k-inverse-pairs-array/629-k-inverse-pairs-array.java
@@ -1,9 +1,8 @@
 class Solution {
-    // private final int MOD = (int)(1e9 + 7);
+    private final int MOD = (int)(1e9 + 7);
     public int kInversePairs(int n, int k) {
         int dp[][] = new int[n + 1][k + 1];
         dp[0][0] = 1;
-        int MOD = 1000000007;
         for (int i = 1; i <= n; ++i) {
             dp[i][0] = 1;
             for (int j = 1; j <= k; ++j) {

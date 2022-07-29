eefee973ac16aceca0924c10857916e9db2c7d53
Shrayansh Baunthiyal <72238316+shrayansh95@users.noreply.github.com>
2022-07-29 12:34:31 +0530

    Time: 1 ms (88.90%), Space: 53.8 MB (42.24%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.java
+++ 785-is-graph-bipartite/785-is-graph-bipartite.java
@@ -3,20 +3,35 @@ class Solution {
         int n = graph.length;
         int color[] = new int[n];
         Arrays.fill(color, -1);
-        color[0] = 0;
         for (int i = 0; i < n; ++i) 
-            if (!dfs(i, graph, color))
+            if (color[i] == -1 && !bfs(i, graph, color)) 
                 return false;
         return true;
     }
-    private boolean dfs(int i, int graph[][], int color[]) {
-        for (int it : graph[i]) {
-            if (color[it] == -1) {
-                color[it] = color[i] ^ 1;
-                if (!dfs(it, graph, color))
+    // private boolean dfs(int i, int graph[][], int color[]) {
+    //     for (int it : graph[i]) {
+    //         if (color[it] == -1) {
+    //             color[it] = color[i] ^ 1;
+    //             if (!dfs(it, graph, color))
+    //                 return false;
+    //         } else if (color[it] == color[i]) 
+    //             return false;
+    //     }
+    //     return true;
+    // }
+    private boolean bfs(int i, int graph[][], int color[]) {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(i);
+        color[i] = 0;
+        while (!q.isEmpty()) {
+            int temp = q.poll();
+            for (int it : graph[temp]) {
+                if (color[it] == -1) {
+                    color[it] = color[temp] ^ 1;
+                    q.add(it);
+                } else if (color[it] == color[temp])
                     return false;
-            } else if (color[it] == color[i]) 
-                return false;
+            }
         }
         return true;
     }

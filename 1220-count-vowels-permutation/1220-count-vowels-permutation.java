020cf74ff02835997d9e1f28b64c5ee6419ed75a
Shrayansh Baunthiyal <72238316+shrayansh95@users.noreply.github.com>
2022-08-07 07:49:25 +0530

    Time: 10 ms (98.17%), Space: 41.4 MB (70.39%) - LeetHub

--- 1220-count-vowels-permutation/1220-count-vowels-permutation.java
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.java
@@ -1,10 +1,10 @@
 class Solution {
     private final int mod = (int)(1e9 + 7);
     public int countVowelPermutation(int n) {
-        long prevA = 1, prevE = 1, prevI = 1, prevO = 1, prevU = 1;
-        long curA = 0, curE = 0, curI = 0, curO = 0, curU = 0;
+        int prevA = 1, prevE = 1, prevI = 1, prevO = 1, prevU = 1;
+        int curA = 0, curE = 0, curI = 0, curO = 0, curU = 0;
         for (int i = 2; i <= n; ++i) {
-            curA = (prevE + prevI + prevU) % mod;
+            curA = (((prevE + prevI) % mod) + prevU) % mod;
             curE = (prevA + prevI) % mod;
             curI = (prevE + prevO) % mod;
             curO = prevI % mod;
@@ -15,6 +15,7 @@ class Solution {
             prevO = curO;
             prevU = curU;
         }
-        return (int)((prevA + prevE + prevI + prevO + prevU) % mod);
+        return ((((prevA + prevE) % mod + prevI) % mod + prevO) % mod + prevU)
+            % mod;
     }
 }
